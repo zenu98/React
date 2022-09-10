542becb313a1dd51feee11ae83773ef33a87abdc
SeoJaeHee <zenu98@naver.com>
2022-09-10 21:00:01 +0900

    study

---
+++ hello-react/src/EventPractice.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+class EventPractice extends Component {
+  state = {
+    message: "",
+    username: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleClick = (e) => {
+    alert(this.state.username + ": " + this.state.message);
+    this.setState({
+      message: "",
+      username: "",
+    });
+  };
+
+  handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      this.handleClick();
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>이벤트 연습</h1>
+        <input
+          type="text"
+          name="username"
+          placeholder="사용자명"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="message"
+          placeholder="아무거나"
+          value={this.state.message}
+          onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
+        />
+        <button onClick={this.handleClick}>확인</button>
+      </div>
+    );
+  }
+}
+
+export default EventPractice;
---
+++ hello-react/src/Say.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const Say = () => {
+  const [message, setMessage] = useState("");
+  const onClickEnter = () => setMessage("안녕하세요");
+  const onClickLeave = () => setMessage("안녕히가세요");
+
+  const [color, setColor] = useState("black");
+  const onClickColor = () => setColor("green");
+
+  return (
+    <div>
+      <button onClick={onClickEnter}>입장</button>
+      <button onClick={onClickLeave}>퇴장</button>
+      <h1 style={{ color }}>{message}</h1>
+
+      <button style={{ color: "red" }} onClick={() => setColor("red")}>
+        빨간색
+      </button>
+      <button style={{ color: "blue" }} onClick={() => setColor("blue")}>
+        파란색
+      </button>
+      <button onClick={onClickColor}>초록색</button>
+    </div>
+  );
+};
+export default Say;
