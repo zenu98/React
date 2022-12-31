2dc465fc9533168172df503bbf40324309599c81
SeoJaeHee <zenu98@naver.com>
2022-12-31 23:21:52 +0900

    firebase fetch

--- toy-project/word-puzzle/src/components/Td.js
+++ toy-project/word-puzzle/src/components/Td.js
@@ -1,41 +1,78 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import classes from "./Td.module.css";
 
 const Td = (props) => {
   const { onClick } = props;
+  const [dataList, setDataList] = useState([]);
   const [data, setData] = useState([]);
   const [wordArr, setWordArr] = useState([]);
   const [disabledBtn, setDisabledBtn] = useState([]);
   const [buttonCss, setButtonCss] = useState([]);
   console.log(buttonCss);
   console.log(disabledBtn);
+  console.log(dataList);
+  console.log(wordArr);
+
+  const dataListHandler = useCallback((data) => {
+    setDataList(data);
+  }, []);
 
   useEffect(() => {
-    console.log("useeffect");
     const dataArr = [];
-    const DUMMY = [
-      { id: 1, word: "바나나" },
-      { id: 2, word: "사과" },
-      { id: 3, word: "전화번호" },
-      { id: 4, word: "모자" },
-      { id: 5, word: "마우스" },
-      { id: 6, word: "휴지" },
-      { id: 7, word: "감자" },
-    ];
-    for (let k = 0; k < DUMMY.length; k++) {
-      for (let i = 0; i <= DUMMY[k].word.length - 1; i++) {
-        const chr = DUMMY[k].word.substring(i, i + 1);
-        if (dataArr.length < 16) {
-          dataArr.push({
-            id: Math.random().toString(),
-            word: chr,
+    fetch("https://word-puzzle-efb93-default-rtdb.firebaseio.com/animals.json")
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedAnimals = [];
+        for (const key in responseData) {
+          loadedAnimals.push({
+            id: key,
+            name: responseData[key].name,
+            description: responseData[key].description,
           });
         }
-      }
-    }
-    dataArr.sort(() => Math.random() - 0.5);
-    setData(dataArr);
-  }, []);
+        dataListHandler(loadedAnimals);
+
+        for (let k = 0; k < loadedAnimals.length; k++) {
+          for (let i = 0; i <= loadedAnimals[k].name.length - 1; i++) {
+            const chr = loadedAnimals[k].name.substring(i, i + 1);
+
+            dataArr.push({
+              id: Math.random().toString(),
+              word: chr,
+            });
+          }
+        }
+        dataArr.sort(() => Math.random() - 0.5);
+        setData(dataArr);
+      });
+  }, [dataListHandler]);
+
+  // useEffect(() => {
+  //   console.log("useeffect");
+
+  //   const DUMMY = [
+  //     { id: 1, word: "바나나" },
+  //     { id: 2, word: "사과" },
+  //     { id: 3, word: "전화번호" },
+  //     { id: 4, word: "모자" },
+  //     { id: 5, word: "마우스" },
+  //     { id: 6, word: "휴지" },
+  //     { id: 7, word: "감자" },
+  //   ];
+  //   for (let k = 0; k < DUMMY.length; k++) {
+  //     for (let i = 0; i <= DUMMY[k].word.length - 1; i++) {
+  //       const chr = DUMMY[k].word.substring(i, i + 1);
+  //       if (dataArr.length < 16) {
+  //         dataArr.push({
+  //           id: Math.random().toString(),
+  //           word: chr,
+  //         });
+  //       }
+  //     }
+  //   }
+  //   dataArr.sort(() => Math.random() - 0.5);
+  //   setData(dataArr);
+  // }, []);
 
   useEffect(() => {
     console.log("useEffect");
@@ -48,13 +85,7 @@ const Td = (props) => {
   const clickHandler = (e) => {
     console.log("click");
 
-    const idList = [];
-    for (const key in wordArr) {
-      idList.push(wordArr[key].id);
-    }
-    console.log(idList);
-
-    if (idList.includes(e.target.name)) {
+    if (wordArr.some((item) => item.id === e.target.name)) {
       setButtonCss((prev) => prev.filter((a) => a !== e.target.name));
       setWordArr((prev) => prev.filter((a) => a.id !== e.target.name));
     } else {
@@ -69,7 +100,7 @@ const Td = (props) => {
 
   const submitHanlder = (e) => {
     e.preventDefault();
-    if (props.word === "사과") {
+    if (dataList.some((item) => item.name === props.word)) {
       buttonCss.map((id) => {
         return (
           setWordArr((prev) => prev.filter((a) => a.id !== id)),
@@ -94,7 +125,9 @@ const Td = (props) => {
             key={item.id}
             value={item.word}
             className={`${
-              buttonCss.includes(item.id) ? classes.datas : classes.data
+              wordArr.some((a) => a.id === item.id)
+                ? classes.datas
+                : classes.data
             }`}
             onClick={clickHandler}
           >
