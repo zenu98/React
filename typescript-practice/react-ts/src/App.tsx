8b008755233a4916cd1a6944f97de76423864ce1
SeoJaeHee <zenu98@naver.com>
2023-01-15 21:39:04 +0900

    ts

---
+++ typescript-practice/react-ts/src/App.tsx
@@ -0,0 +1,13 @@
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
+function App() {
+  const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+
+  return (
+    <div>
+      <Todos items={todos} />
+    </div>
+  );
+}
+
+export default App;
