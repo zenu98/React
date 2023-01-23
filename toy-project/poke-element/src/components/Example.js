6af6578f2c2e66e114f8d780abb2f9dcfada6122
SeoJaeHee <zenu98@naver.com>
2023-01-23 19:20:32 +0900

    123

--- toy-project/poke-element/src/components/Example.js
+++ toy-project/poke-element/src/components/Example.js
@@ -1,5 +1,27 @@
 import React, { useState } from "react";
 import classes from "./Example.module.css";
+import { GoFlame } from "react-icons/go";
+import { IoWater, IoEllipseSharp } from "react-icons/io5";
+import {
+  GiFrozenOrb,
+  GiHighGrass,
+  GiPoisonBottle,
+  GiEarthSpit,
+  GiLibertyWing,
+  GiStoneBlock,
+  GiFairyWings,
+  GiEvilEyes,
+} from "react-icons/gi";
+import { AiFillThunderbolt } from "react-icons/ai";
+import {
+  FaFistRaised,
+  FaDragon,
+  FaBug,
+  FaUikit,
+  FaGhost,
+  FaBullseye,
+} from "react-icons/fa";
+import { MdCatchingPokemon } from "react-icons/md";
 
 const Example = (props) => {
   const [toggle, setToggle] = useState(false);
@@ -10,69 +32,67 @@ const Example = (props) => {
     <div className={classes.wrapper}>
       <div className={classes["menu-btn"]} onClick={clickhandler}>
         <div className={classes.btn}>
-          <span></span>
-          <span></span>
-          <span></span>
+          <MdCatchingPokemon />
         </div>
       </div>
 
       <div className={classes["icons-wrapper"]}>
         <div className={classes.icons}>
           <div className={`${classes.icon} ${toggle === true && classes.show}`}>
-            <i className={classes["fa fa-facebook"]}></i>
+            <GoFlame className={classes["icon-fire"]} />
           </div>
           <div className={`${classes.icon} ${toggle === true && classes.show}`}>
-            <i className={classes["fa fa-twitter"]}></i>
+            <IoWater className={classes["icon-water"]} />
           </div>
           <div className={`${classes.icon} ${toggle === true && classes.show}`}>
-            <i className={classes["fa fa-pinterest-p"]}></i>
+            <AiFillThunderbolt className={classes["icon-electric"]} />
           </div>
           <div className={`${classes.icon} ${toggle === true && classes.show}`}>
-            <i className={classes["fa fa-envelope"]}></i>
+            <GiFrozenOrb className={classes["icon-ice"]} />
           </div>
           <div className={`${classes.icon} ${toggle === true && classes.show}`}>
-            <i className={classes["fa fa-phone"]}></i>
+            <GiHighGrass className={classes["icon-grass"]} />
           </div>
           <div className={`${classes.icon} ${toggle === true && classes.show}`}>
-            <i className={classes["fa fa-vk"]}></i>
+            <FaFistRaised className={classes["icon-fighting"]} />
           </div>
           <div className={`${classes.icon} ${toggle === true && classes.show}`}>
-            <i className={classes["fa fa-google"]}></i>
-          </div>
-
-          <div className={`${classes.icon} ${toggle === true && classes.show}`}>
-            <i className={classes["fa fa-google"]}></i>
-          </div>
-          <div className={`${classes.icon} ${toggle === true && classes.show}`}>
-            <i className={classes["fa fa-google"]}></i>
-          </div>
-          <div className={`${classes.icon} ${toggle === true && classes.show}`}>
-            <i className={classes["fa fa-google"]}></i>
-          </div>
-          <div className={`${classes.icon} ${toggle === true && classes.show}`}>
-            <i className={classes["fa fa-google"]}></i>
-          </div>
-          <div className={`${classes.icon} ${toggle === true && classes.show}`}>
-            <i className={classes["fa fa-google"]}></i>
-          </div>
-          <div className={`${classes.icon} ${toggle === true && classes.show}`}>
-            <i className={classes["fa fa-google"]}></i>
-          </div>
-          <div className={`${classes.icon} ${toggle === true && classes.show}`}>
-            <i className={classes["fa fa-google"]}></i>
+            <GiPoisonBottle className={classes["icon-poison"]} />
           </div>
 
           <div className={`${classes.icon} ${toggle === true && classes.show}`}>
-            <i className={classes["fa fa-youtube"]}></i>
+            <GiEarthSpit className={classes["icon-ground"]} />
           </div>
           <div className={`${classes.icon} ${toggle === true && classes.show}`}>
-            <i className={classes["fa fa-youtube"]}></i>
+            <GiLibertyWing className={classes["icon-flying"]} />
           </div>
           <div className={`${classes.icon} ${toggle === true && classes.show}`}>
-            <i className={classes["fa fa-youtube"]}></i>
+            <GiStoneBlock className={classes["icon-rock"]} />
           </div>
           <div className={`${classes.icon} ${toggle === true && classes.show}`}>
-            <i className={classes["fa fa-youtube"]}></i>
+            <FaDragon className={classes["icon-dragon"]} />
+          </div>
+          <div className={`${classes.icon} ${toggle === true && classes.show}`}>
+            <FaBug className={classes["icon-bug"]} />
+          </div>
+          <div className={`${classes.icon} ${toggle === true && classes.show}`}>
+            <FaBullseye className={classes["icon-psychic"]} />
+          </div>
+          <div className={`${classes.icon} ${toggle === true && classes.show}`}>
+            <FaUikit className={classes["icon-steel"]} />
+          </div>
+
+          <div className={`${classes.icon} ${toggle === true && classes.show}`}>
+            <FaGhost className={classes["icon-ghost"]} />
+          </div>
+          <div className={`${classes.icon} ${toggle === true && classes.show}`}>
+            <GiEvilEyes className={classes["icon-dark"]} />
+          </div>
+          <div className={`${classes.icon} ${toggle === true && classes.show}`}>
+            <GiFairyWings className={classes["icon-fairy"]} />
+          </div>
+          <div className={`${classes.icon} ${toggle === true && classes.show}`}>
+            <IoEllipseSharp className={classes["icon-normal"]} />
           </div>
         </div>
       </div>
