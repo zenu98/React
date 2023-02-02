e670e4afff2f001a0c380ee88d0f576bb61d80c8
SeoJaeHee <zenu98@naver.com>
2023-02-02 23:03:44 +0900

    123

--- toy-project/poke-element/src/components/Layout/Layout.js
+++ toy-project/poke-element/src/components/Layout/Layout.js
@@ -1,29 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import ReactFullpage from "@fullpage/react-fullpage";
+import React, { useRef } from "react";
+import { FullPage, Slide } from "react-full-page";
 import classes from "./Layout.module.css";
 import Main from "./Main";
 import ResultPage from "./ResultPage";
 import TypePage from "./TypePage";
 
 const Layout = () => {
-  <ReactFullpage
-    //fullpage options
-    licenseKey={"YOUR_KEY_HERE"}
-    scrollingSpeed={1000} /* Options here */
-    render={({ state, fullpageApi }) => {
-      return (
-        <ReactFullpage.Wrapper>
-          <div className="section">
-            <Main />
-          </div>
-          <div className="section">
-            <TypePage />
-          </div>
-        </ReactFullpage.Wrapper>
-      );
-    }}
-  />;
+  const ref = useRef(null);
+
+  const onControlsClick = () => {
+    ref.current.scrollToSlide(1);
+  };
+  return (
+    <FullPage ref={ref}>
+      <Slide>
+        <Main onClick={onControlsClick} />
+      </Slide>
+      <Slide>
+        <TypePage />
+      </Slide>
+      <Slide>
+        <ResultPage />
+      </Slide>
+    </FullPage>
+  );
 };
-ReactDOM.render(<Layout />, document.getElementById("root"));
 export default Layout;
--- toy-project/poke-element/src/components/Layout/Main.js
+++ toy-project/poke-element/src/components/Layout/Main.js
@@ -1,6 +1,20 @@
 import classes from "./Main.module.css";
-const Main = () => {
-  return <div className={classes.background}></div>;
+const Main = (props) => {
+  return (
+    <div className={classes.background}>
+      <div className={classes.titlebox}>
+        <p className={classes.title} data-text="포켓몬">
+          포켓몬
+        </p>
+        <p className={classes.title}>상성 계산기</p>
+        <button type="button" onClick={props.onClick}>
+          toggle last slide
+        </button>
+      </div>
+
+      <img id={classes.poketmon} alt="이상해씨" src="img/이상해씨.png" />
+    </div>
+  );
 };
 
 export default Main;
