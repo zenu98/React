a2440ac2497b352bfb028c23275db411d4b18591
SeoJaeHee <zenu98@naver.com>
2023-02-04 00:20:42 +0900

    123

--- toy-project/poke-element/src/components/Layout/ResultPage.js
+++ toy-project/poke-element/src/components/Layout/ResultPage.js
@@ -2,13 +2,18 @@ import classes from "./ResultPage.module.css";
 import { useSelector } from "react-redux";
 const ResultPage = () => {
   const result = useSelector((state) => state.result.typesArr);
-
   const twoArr = [];
-  const typesHandler = () => {
-    for (let key in result[0]) {
-      twoArr.push(key);
+  const oneArr = [];
+  console.log(result);
+  for (let i = 0; i < result.length; i++) {
+    if (result[i][1] === 2) {
+      twoArr.push(result[i][0]);
+    } else if (result[i][1] === 1) {
+      oneArr.push(result[i][0]);
     }
-  };
+  }
+  console.log(oneArr);
+  console.log(twoArr);
 
   return <div className={classes.background}></div>;
 };
