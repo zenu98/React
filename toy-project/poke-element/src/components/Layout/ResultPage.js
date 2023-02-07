e2013329e0d1b6232eeb619afbb42e3987fcf3f9
SeoJaeHee <zenu98@naver.com>
2023-02-07 23:56:46 +0900

    defense type

--- toy-project/poke-element/src/components/Layout/ResultPage.js
+++ toy-project/poke-element/src/components/Layout/ResultPage.js
@@ -2,20 +2,98 @@ import classes from "./ResultPage.module.css";
 import { useSelector } from "react-redux";
 const ResultPage = () => {
   const result = useSelector((state) => state.result.typesArr);
-  const twoArr = [];
-  const oneArr = [];
+  const twoArrOffense = [];
+  const oneArrOffense = [];
+  const pointFiveArrOffense = [];
+  const zeroOffense = [];
+
   console.log(result);
   for (let i = 0; i < result.length; i++) {
     if (result[i][1] === 2) {
-      twoArr.push(result[i][0]);
+      twoArrOffense.push(result[i][0]);
     } else if (result[i][1] === 1) {
-      oneArr.push(result[i][0]);
+      oneArrOffense.push(result[i][0]);
+    } else if (result[i][1] === 0.5) {
+      pointFiveArrOffense.push(result[i][0]);
+    } else if (result[i][1] === 0) {
+      zeroOffense.push(result[i][0]);
     }
   }
-  console.log(oneArr);
-  console.log(twoArr);
 
-  return <div className={classes.background}></div>;
+  return (
+    <div className={classes.background}>
+      <div className={classes.container}>
+        <div className={classes.box}>
+          <div className={classes.textbox}>
+            <div className={classes.title}>공격</div>
+            {twoArrOffense.length > 0 && (
+              <div className={classes.content}>
+                <div className={classes.number}>2x</div>
+                <div>
+                  {twoArrOffense.map((item) => (
+                    <img
+                      className={classes.type}
+                      src={`img/${item}.png`}
+                      alt={`${item}`}
+                      key={`${item}`}
+                    />
+                  ))}
+                </div>
+              </div>
+            )}
+            {oneArrOffense.length > 0 && (
+              <div className={classes.content}>
+                <div className={classes.number}>1x</div>
+                <div>
+                  {oneArrOffense.map((item) => (
+                    <img
+                      className={classes.type}
+                      src={`img/${item}.png`}
+                      alt={`${item}`}
+                      key={`${item}`}
+                    />
+                  ))}
+                </div>
+              </div>
+            )}
+            {pointFiveArrOffense.length > 0 && (
+              <div className={classes.content}>
+                <div className={classes.number}>0.5x</div>
+                <div>
+                  {pointFiveArrOffense.map((item) => (
+                    <img
+                      className={classes.type}
+                      src={`img/${item}.png`}
+                      alt={`${item}`}
+                      key={`${item}`}
+                    />
+                  ))}
+                </div>
+              </div>
+            )}{" "}
+            {zeroOffense.length > 0 && (
+              <div className={classes.content}>
+                <div className={classes.number}>0x</div>
+                <div>
+                  {zeroOffense.map((item) => (
+                    <img
+                      className={classes.type}
+                      src={`img/${item}.png`}
+                      alt={`${item}`}
+                      key={`${item}`}
+                    />
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+          <div className={classes.textbox}>
+            <p className={classes.title}>방어</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ResultPage;
