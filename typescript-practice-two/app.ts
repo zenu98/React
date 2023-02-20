08e8c0e2f2fd1b0decc25bc1b60f7c7e30d650ce
SeoJaeHee <zenu98@naver.com>
2023-02-21 08:57:20 +0900

    ts

---
+++ typescript-practice-two/app.ts
@@ -0,0 +1,13 @@
+const person = {
+  name: "seo",
+  age: 21,
+  hobbies: ["sports", "music"],
+};
+
+let arr: string[];
+arr = ["sports"];
+
+console.log(person.name);
+for (const i of person.hobbies) {
+  console.log(i);
+}
