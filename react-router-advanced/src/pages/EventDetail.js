2f0c19c1e0d2f3f0a027d947bf8b0d3558bddc0d
SeoJaeHee <zenu98@naver.com>
2023-03-21 20:22:51 +0900

    react router

---
+++ react-router-advanced/src/pages/EventDetail.js
@@ -0,0 +1,23 @@
+import { useLoaderData, json } from "react-router-dom";
+import EventItem from "../components/EventItem";
+function EventDetailPage() {
+  const data = useLoaderData();
+  return <EventItem event={data.event} />;
+}
+
+export default EventDetailPage;
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + id);
+  if (!response.ok) {
+    throw json(
+      { message: "Could not fetch" },
+      {
+        status: 500,
+      }
+    );
+  } else {
+    return response;
+  }
+}
---
+++ react-router-advanced/src/pages/Events.js
@@ -0,0 +1,32 @@
+import { useLoaderData, json } from "react-router-dom";
+import EventsList from "../components/EventsList";
+
+function EventsPage() {
+  const data = useLoaderData();
+  //   if(data.isError){
+  //     return <p>{data.message}</p>
+  //   }
+  const events = data.events;
+  return <EventsList events={events} />;
+}
+
+export default EventsPage;
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // // return {isError:true, message:'Count dont fetch events.'};
+    // throw new Response(JSON.stringify({ message: "could not fetch events." }), {
+    //   status: 500,
+    // });
+    throw json(
+      { message: "could not fetch events." },
+      {
+        status: 500,
+      }
+    );
+  } else {
+    return response;
+  }
+}
